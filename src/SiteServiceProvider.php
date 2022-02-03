fa48a2f02163dd86d95154dfc04d61334a11e052
Michel Calisto <calisto.michel@gmail.com>
2022-02-03 14:40:26 -0300

    added migrations

--- src/SiteServiceProvider.php
+++ src/SiteServiceProvider.php
@@ -49,19 +49,9 @@ final class SiteServiceProvider extends Provider
             __DIR__.'/../resources/views' => resource_path('resources/js/Pages'),
         ], 'site-views');
 
-        // $this->publishes([
-        //     __DIR__.'/../resources/views' => resource_path('views/vendor/jetstream'),
-        // ], 'jetstream-views');
-
-        // $this->publishes([
-        //     __DIR__.'/../database/migrations/2014_10_12_000000_create_users_table.php' => database_path('migrations/2014_10_12_000000_create_users_table.php'),
-        // ], 'jetstream-migrations');
-
-        // $this->publishes([
-        //     __DIR__.'/../database/migrations/2020_05_21_100000_create_teams_table.php' => database_path('migrations/2020_05_21_100000_create_teams_table.php'),
-        //     __DIR__.'/../database/migrations/2020_05_21_200000_create_team_user_table.php' => database_path('migrations/2020_05_21_200000_create_team_user_table.php'),
-        //     __DIR__.'/../database/migrations/2020_05_21_300000_create_team_invitations_table.php' => database_path('migrations/2020_05_21_300000_create_team_invitations_table.php'),
-        // ], 'jetstream-team-migrations');
+        $this->publishes([
+            __DIR__.'/../database/migrations/2022_01_20_000000_create_sites_table.php' => database_path('migrations/2022_01_20_000000_create_sites_table.php'),
+        ], 'site-migrations');
 
         // $this->publishes([
         //     __DIR__.'/../routes/'.config('jetstream.stack').'.php' => base_path('routes/jetstream.php'),
