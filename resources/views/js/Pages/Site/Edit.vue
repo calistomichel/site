be40ab61b2ac5876e6ce5e4cb411a876b54beb5b
Michel Calisto <calisto.michel@gmail.com>
2022-02-23 14:53:39 -0300

    update site views

--- resources/views/js/Pages/Site/Edit.vue
+++ resources/views/js/Pages/Site/Edit.vue
@@ -1,13 +1,13 @@
 <template>
-    <app-layout title="Create Image">
+    <app-layout title="Edit Site">
         <template #header>
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
                 Edit Site
             </h2>
         </template>
 
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div>
+            <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
 
                     <div class="p-6 sm:px-20 bg-white border-b border-gray-200">
