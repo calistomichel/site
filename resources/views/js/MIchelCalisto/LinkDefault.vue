3a9fa933472e0bd563ee5ae2c9505fea2f54ebcd
Michel Calisto <calisto.michel@gmail.com>
2022-02-24 21:40:26 -0300

    added modal

---
+++ resources/views/js/MIchelCalisto/LinkDefault.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <button :type="type" v-if="as == 'button'">
+            <slot></slot>
+        </button>
+
+        <Link :href="href" :type="type" v-else>
+            <slot></slot>
+        </Link>
+    </div>
+</template>
+
+<script>
+    import { defineComponent } from 'vue';
+    import { Link } from '@inertiajs/inertia-vue3';
+
+    export default defineComponent({
+        components: {
+            Link,
+        },
+        props: {
+            type: {
+                type: String,
+                default: 'button',
+            },
+            href: {
+                default: 'href',
+            },
+            as: {
+                default: 'as',
+            }
+        }
+    })
+</script>
--- resources/views/js/Pages/Site/Index.vue
+++ resources/views/js/Pages/Site/Index.vue
@@ -36,18 +36,18 @@
                                         {{ item.dns }}
                                     </td>
                                     <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                        <Link :href="route('sites.edit', item.id)" type="button">
+                                        <link-default :href="route('sites.edit', item.id)">
                                             <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 text-blue-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
                                             </svg>
-                                        </Link>
+                                        </link-default>
                                     </td>
                                     <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                        <Link method="delete" as="button" :href="route('sites.destroy', item.id)" type="button">
+                                        <link-default @click="confirmDeletion(item.id)" as="button">
                                             <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 text-red-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
                                             </svg>
-                                        </Link>
+                                        </link-default>
                                     </td>
                                 </tr>
                             </tbody>
@@ -57,26 +57,74 @@
 
             </div>
         </div>
+
+        <jet-dialog-modal :show="confirmingDeletion" @close="closeModal">
+            <template #title>
+                Delete Site
+            </template>
+
+            <template #content>
+                ¿Estás seguro de que deseas eliminar esta imagen? Una vez que se elimine esta imagen, se eliminará permanentemente.
+            </template>
+
+            <template #footer>
+                <jet-secondary-button @click="closeModal">
+                    Cancel
+                </jet-secondary-button>
+                <jet-danger-button class="ml-2" @click="destroy">
+                    Delete
+                </jet-danger-button>
+            </template>
+        </jet-dialog-modal>
     </app-layout>
 </template>
 
 <script>
     import { defineComponent } from 'vue'
     import AppLayout from '@/Layouts/AppLayout.vue'
-    import { Link } from '@inertiajs/inertia-vue3'
     import IndexTemplate from '@/MichelCalisto/IndexTemplate.vue'
     import ButtonPrimary from '@/MichelCalisto/ButtonPrimary.vue'
+    import LinkDefault from '@/MichelCalisto/LinkDefault.vue'
+    import JetDialogModal from '@/Jetstream/DialogModal.vue'
+    import JetDangerButton from '@/Jetstream/DangerButton.vue'
+    import JetSecondaryButton from '@/Jetstream/SecondaryButton.vue'
 
     export default defineComponent({
         components: {
             AppLayout,
-            Link,
             IndexTemplate,
             ButtonPrimary,
+            LinkDefault,
+            JetDialogModal,
+            JetDangerButton,
+            JetSecondaryButton,
         },
         props: {
             items: Array,
             count: Number,
         },
+        data() {
+            return {
+                confirmingDeletion: false,
+                id: null,
+            }
+        },
+        methods: {
+            confirmDeletion(id) {
+                this.confirmingDeletion = true;
+                this.id = id;
+            },
+            destroy() {
+                if (this.id !== null) {
+                    this.$inertia.delete(this.route('sites.destroy', this.id), {
+                        onSuccess: () => this.closeModal(),
+                    })
+                }
+            },
+            closeModal() {
+                this.confirmingDeletion = false;
+                this.id = null;
+            },
+        }
     })
 </script>
